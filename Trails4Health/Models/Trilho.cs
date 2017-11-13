ad7d44bec6a1a96e4b2a3d86f07b25d221440d5f
Nuno <yo_325@yahoo.com>
2017-11-13 20:45:23 +0000

    P.I. Completadas classes Trilho + ITrails4HealthRepository
    Tempo: 15 min.

---
+++ Trails4Health/Models/Trilho.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trails4Health.Models
+{
+    public class Trilho
+    {
+        public int ID_Trilho { get; set; }
+        public string Nome_Trilho { get; set; }
+        public string Foto_Trilho { get; set; } // +++ endereço da foto??
+        public string Detalhes_Trilho { get; set; }
+        public bool Desativado_Trilho { get; set; }
+        public string Inicio_Trilho { get; set; }
+        public string Fim_Trilho { get; set; }
+        public string Distancia_Trilho { get; set; } // int ??
+        //public int ID_Dificuldade { get; set; } // FK /*??*/
+
+    }
+}
