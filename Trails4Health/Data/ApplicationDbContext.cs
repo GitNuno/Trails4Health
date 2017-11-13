b628892ceb448ad4398e213a7b0344cf2b7b779e
Nuno <yo_325@yahoo.com>
2017-11-13 21:36:53 +0000

    P.I. Completei classes ApplicationDbContext + EFTrails4HealthRepository
    Tempo: 15 min.

--- Trails4Health/Data/ApplicationDbContext.cs
+++ Trails4Health/Data/ApplicationDbContext.cs
@@ -7,8 +7,16 @@ using System.Threading.Tasks;
 //namespace Trails4Health.Data - orig.
 namespace Trails4Health.Models
 {
+    // class DbContext mapea BD
+    // instancio esta classe na minha [EFRepository:IRepository] 
     public class ApplicationDbContext : DbContext
     {
+        // base(options) mm que super: cria BD com as options
+        // permite-me ter acessso ao que está no modelo
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        // config. base dados com os modelos: digo onde vou passar a BD
+        // tenho de fazer para cada tabela 
+        public DbSet<Trilho> Trilhos { get; set; }
+        // +++ aqui aparecem os DbSet<MyModels>  ???    }
     }
-}
