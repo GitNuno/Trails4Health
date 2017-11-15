2681c73551b138adcf8c8555366c96ba29b304da
Nuno <yo_325@yahoo.com>
2017-11-15 13:02:25 +0000

    P.I. Atualizada documentão do projecto a partir da leitura do livro
    .Tempo: 30min.

--- Trails4Health/Startup.cs
+++ Trails4Health/Startup.cs
@@ -29,25 +29,23 @@ namespace Trails4Health
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
-            services.AddMvc();
-
             //  *** se quiser mudar repositorio...
             //- assim não preciso de mudar mais nada que nao seja FakeProductRepository
             // services.AddTransient<ITrails4HealthRepository, FakeProductRepository>(); // mudado!!
 
-            // serviço 
-            // se fizesse com classe em vez de interface: ITrails4HealthRepository ...
-            services.AddTransient<ITrails4HealthRepository, EFTrails4HealthRepository>();
-
-            // configurar EF
-            services.AddDbContext<ApplicationDbContext>(
-              options => options.UseSqlServer(
+            /* configurar a app para usar a ConnectionStringTrails4Health e ligar á B.D.*/
+            services.AddDbContext<ApplicationDbContext>( options => options.UseSqlServer
+              (
                   // vou por nome da string connection do appsettings.jason
                   Configuration.GetConnectionString("ConnectionStringTrails4Health")
               )
           );
+            /* quando são criados os componentes que usam ITrails4HealthRepository (no momento apenas Trilhos(controler)) 
+               recebem um objecto EFTrails4HealthRepository, este objecto providencia aos componentes acesso á B.D. */
+            services.AddTransient<ITrails4HealthRepository, EFTrails4HealthRepository>();
 
+            // Add framework services.
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
