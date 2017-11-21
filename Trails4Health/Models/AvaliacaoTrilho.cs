d8727d898970a7a25b386453dd0d6a43059ee89d
Nuno Clodic <nunoclodic_so10@hotmail.com>
2017-11-21 13:03:33 +0000

    ProgInternet CriacaoClasse
    ProgInternet CriacaoClasse
    Nota: Finalmente Estou conseguindo trabalhar no meu computador e so agora comecei por trabalhar, o primeiro commit so sao criacao de duas classes.
    tempo: 10 min

---
+++ Trails4Health/Models/AvaliacaoTrilho.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trails4Health.Models
+{
+    public class AvaliacaoTrilho
+    {
+       // public int TrilhoID { get; set; } 
+        //public int RespostaID{ get; set; }
+        //public Date DataAvaliacao { get; set; } 
+        
+    }
+}
---
+++ Trails4Health/Models/InserirPerguntaAvaliacaoTrilho.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trails4Health.Models
+{
+    public class InserirPerguntaAvaliacaoTrilho
+    {
+        // public int PerguntaID { get; set; } 
+        //public int TrilhoID{ get; set; }
+        //public Date Formulario { get; set; } 
+    }
+}
