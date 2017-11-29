bdf3b48e4f09ad44d8174d3c4d59e7683908b2d5
Nuno <yo_325@yahoo.com>
2017-11-29 21:49:04 +0000

    P.I. Criada ViewModelTrilho + Falhada inserção de TrilhoID na tabela EstadoTrilho
    Nota:
    ERRO: The INSERT statement conflicted with the FOREIGN KEY constraint "FK_EstadoTrilhos_Trilhos_TrilhoID". The conflict occurred in database "Trails4Health", table "dbo.Trilhos", column 'TrilhoID'.
    Tempo: 1h

--- Project - Trails4Health/Trails4Health/Controllers/TrilhoesController.cs
+++ Project - Trails4Health/Trails4Health/Controllers/TrilhoesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Trails4Health.Models;
+using Trails4Health.Models.ViewModels;
 
 namespace Trails4Health.Controllers
 {
@@ -50,6 +51,7 @@ namespace Trails4Health.Controllers
             // viewBag recebe valores do tipo ViewData["DificuldadeID"] em runTime
             // Nome é o campo de texto referente ao DificuldadeID
             ViewData["DificuldadeID"] = new SelectList(_context.Dificuldades, "DificuldadeID", "Nome");
+            ViewData["EstadoID"] = new SelectList(_context.Estados, "EstadoID", "Nome");
             return View();
         }
 
@@ -58,7 +60,8 @@ namespace Trails4Health.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("TrilhoID,Nome,Inicio,Fim,Detalhes,Distancia,Foto,Desativado,DificuldadeID")] Trilho trilho, EstadoTrilho estadoTrilho)
+        public async Task<IActionResult> Create([Bind("TrilhoID,TrilhoNome,TrilhoInicio,TrilhoFim,TrilhoDetalhes,TrilhoDistancia,TrilhoFoto," +
+                                                      "TrilhoDesativado,DificuldadeID,EstadoID")] ViewModelTrilho VMTrilho)
         {
  
             if (ModelState.IsValid)
@@ -67,13 +70,36 @@ namespace Trails4Health.Controllers
                 //estadoTrilho.TrilhoID = trilho.TrilhoID; //se fizer Ex: 7 funciona !
                 //estadoTrilho.DataInicio = DateTime.Now;
                 //estadoTrilho.EstadoID = 1;
+
+                Trilho trilho = new Trilho
+                {
+                    TrilhoID = VMTrilho.TrilhoID,
+                    Nome = VMTrilho.TrilhoNome,
+                    Inicio = VMTrilho.TrilhoInicio,
+                    Fim = VMTrilho.TrilhoFim,
+                    Distancia = VMTrilho.TrilhoDistancia,
+                    Foto = VMTrilho.TrilhoFoto,
+                    Desativado = VMTrilho.TrilhoDesativado,
+                    Detalhes = VMTrilho.TrilhoDetalhes,
+                    DificuldadeID = VMTrilho.DificuldadeID
+                };
+                
+                EstadoTrilho estadoTrilho = new EstadoTrilho
+                {
+                    TrilhoID = VMTrilho.TrilhoID,
+                    EstadoID = VMTrilho.EstadoID,
+                    DataInicio = DateTime.Now,
+                };
+
                 _context.Add(trilho);
-                //_context.Add(estadoTrilho);
                 await _context.SaveChangesAsync();
+
+                _context.Add(estadoTrilho);
+                await _context.SaveChangesAsync(); // ERRO !!!
                 return RedirectToAction("Index");
             }
-            ViewData["DificuldadeID"] = new SelectList(_context.Dificuldades, "DificuldadeID", "DificuldadeID", trilho.DificuldadeID);
-            return View(trilho);
+            ViewData["DificuldadeID"] = new SelectList(_context.Dificuldades, "DificuldadeID", "DificuldadeID", VMTrilho.DificuldadeID);
+            return View(VMTrilho);
         }
 
         // GET: Trilhoes/Edit/5
---
+++ Project - Trails4Health/Trails4Health/Models/ViewModels/ViewModelTrilho.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trails4Health.Models.ViewModels
+{
+    public class ViewModelTrilho
+    {
+
+        public int TrilhoID;
+
+        [Required(ErrorMessage = "Introduza nome do Trilho")]
+        public string TrilhoNome { get; set; }
+
+        [Required(ErrorMessage = "Introduza inicio do Trilho")]
+        public string TrilhoInicio { get; set; }
+
+        [Required(ErrorMessage = "Introduza fim do Trilho")]
+        public string TrilhoFim { get; set; }
+
+        [Required(ErrorMessage = "Introduza detalhes do Trilho")]
+        public string TrilhoDetalhes { get; set; }
+
+        [Required(ErrorMessage = "Introduza Distancia do Trilho")]
+        public decimal TrilhoDistancia { get; set; }
+
+        [Required(ErrorMessage = "Escolha uma foto")]
+        public string TrilhoFoto { get; set; } // mais tarde vai ser na base dados
+
+        public bool TrilhoDesativado { get; set; } = false;
+
+        public int DificuldadeID { get; set; }
+
+        public int DificuldadeNome { get; set; }
+
+        //
+        public int EstadoID { get; set; }
+
+    }
+}
