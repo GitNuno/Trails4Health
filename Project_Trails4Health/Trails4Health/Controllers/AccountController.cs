1b96a2f1590b5c4cf84b32ee54f2b9d2f913142d
rf500500 <rf500500@gmail.com>
2018-01-10 10:07:58 +0000

    PI Actualização relacionada com autenticação
    - Continuação do projecto "Trails4Health":
    Quando um utilizador se regista, fica com o role "Turista" atribuído por defeito (implementado em "Controllers/AccountController.cs");
    Exemplo de restrição de acesso implementado para as views de "Views/Backoffice/" (implementado em "Controllers/Backoffice.cs"), é necessário estar autenticado (utilizadores com o role Administrador ou Professor). O utilizador com role "Turista" não tem acesso;

--- Project_Trails4Health/Trails4Health/Controllers/AccountController.cs
+++ Project_Trails4Health/Trails4Health/Controllers/AccountController.cs
@@ -230,6 +230,7 @@ namespace Trails4Health.Controllers
                 var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    await _userManager.AddToRoleAsync(user, "Turista"); // quando um utilizador se regista, passa a ter o role "Turista" por defeito
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
