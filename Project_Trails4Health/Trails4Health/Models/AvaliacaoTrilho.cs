1b4849a71fac96f4f42d8c5d7d1d0112b2235269
Nuno <yo_325@yahoo.com>
2018-01-14 10:21:07 +0000

    P.I. ----- SUBSTITUIDA versão antiga por versao Foto VARBINARY --------

---
+++ Project_Trails4Health/Trails4Health/Models/AvaliacaoTrilho.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trails4Health.Models
+{
+    public class AvaliacaoTrilho
+    {
+        public int AvaliacaoTrilhoID { get; set; }
+
+        public Trilho Trilho { get; set; }
+        public int TrilhoID { get; set; }
+
+        public double Avaliacao { get; set; }
+        public int NumeroAvaliacoes { get; set; }
+    }
+}
---
+++ Project_Trails4Health/Trails4Health/Models/RespostaQuestionario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trails4Health.Models
+{
+    public class RespostaQuestionario
+    {
+        public int RespostaQuestionarioID { get; set; }
+
+        public Questionario Questionario { get; set; }
+        public int QuestionarioID { get; set; }
+
+        public Turista Turista { get; set; }
+        public int TuristaID { get; set; }
+
+        public DateTime Data { get; set; }
+
+        public ICollection<AvaliacaoGuia> AvaliacaoGuias { get; set; }
+        public ICollection<AvaliacaoTrilho> AvaliacaoTrilhos { get; set; }
+       // public ICollection<Resposta> Respostas { get; set; }
+    }
+}
