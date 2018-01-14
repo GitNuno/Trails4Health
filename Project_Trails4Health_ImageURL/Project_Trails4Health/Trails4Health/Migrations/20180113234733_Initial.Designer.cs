dbfc7f9bb852ef0d1c04b298226c85a3f3673375
Nuno <yo_325@yahoo.com>
2018-01-14 01:40:11 +0000

    P.I. Corrigidas Views e Layouts
    .Corrigidas Views e Layouts
    Tempo: 30 min.

--- Project_Trails4Health_ImageURL/Project_Trails4Health/Trails4Health/Migrations/20180113234733_Initial.Designer.cs
+++ Project_Trails4Health_ImageURL/Project_Trails4Health/Trails4Health/Migrations/20180113234733_Initial.Designer.cs
@@ -11,7 +11,7 @@ using Trails4Health.Models;
 namespace Trails4Health.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20180113144324_Initial")]
+    [Migration("20180113234733_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
