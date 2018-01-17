32f73cb1c07b7562b282be3fe7287dea99f486b3
Nuno <yo_325@yahoo.com>
2018-01-17 14:51:27 +0000

    P.I. Adicionado ProjectTrails4HealthFotoEndereço

---
+++ ProjectTrails4HealthFotoEndereco/Trails4Health/Models/Questionario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trails4Health.Models
+{
+    public class Questionario
+    {
+        public int QuestionarioID { get; set; }
+        public DateTime DataRespostas { get; set; }
+
+        public ICollection<QuestionarioQuestao> QuestionarioQuestoes { get; set; }
+        //public QuestionarioQuestao QuestionarioQuestao { get; set; }
+        //public RespostaQuestionario RespostaQuestionario { get; set; }
+    }
+}
---
+++ ProjectTrails4HealthFotoEndereco/Trails4Health/Models/Turista.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trails4Health.Models
+{
+    public class Turista
+    {
+        public int TuristaID { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Morada { get; set; }
+        public string Email { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DataNascimento { get; set; }
+
+        [Range(100000000, 999999999)]
+        public int Nif { get; set; }
+
+        public ICollection<Resposta> Respostas { get; set; }
+        //public ICollection<RespostaQuestionario> RespostaQuestionarios { get; set; }
+    }
+}
