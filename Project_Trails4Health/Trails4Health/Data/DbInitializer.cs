5886e06991e457f4d5c111250ce4893932dcec75
rf500500 <rf500500@gmail.com>
2018-01-26 19:49:49 +0000

    PI Alterações relacionadas com as classes guia e turista
    Criados dados fictícios para popular tabelas Guia e Turista;
    Criados constroladores Controllers/GuiasController.cs e Controllers/TuristasController.cs e respectivas Views (scaffolding);

--- Project_Trails4Health/Trails4Health/Data/DbInitializer.cs
+++ Project_Trails4Health/Trails4Health/Data/DbInitializer.cs
@@ -13,16 +13,6 @@ namespace Trails4Health.Data
         {
             context.Database.EnsureCreated();
 
-            // Se não encontrou um registo ...
-            //if (!context.TipoQuestoes.Any())
-            //{
-            //    context.TipoQuestoes.AddRange(
-            //    new TipoQuestao { Nome = "Trilho" },
-            //    new TipoQuestao { Nome = "Guia" }
-            //    );
-            //    context.SaveChanges();
-            //}
-
             if (!context.Opcoes.Any())
             {
                 context.Opcoes.AddRange(
@@ -36,7 +26,7 @@ namespace Trails4Health.Data
             if (!context.Questoes.Any())
             {
                 context.Questoes.AddRange(
-                new Questao { NomeQuestao = "Classifique globalmente este trilho?", Desactivada = false, Descricao = "1 - Horrível | 5 - Excelente", OpcaoID = 3},
+                new Questao { NomeQuestao = "Classifique globalmente este trilho?", Desactivada = false, Descricao = "1 - Horrível | 5 - Excelente", OpcaoID = 3 },
                 new Questao { NomeQuestao = "O trilho continha sinalização adequada?", Desactivada = false, Descricao = "Sim | Não", OpcaoID = 1 },
                 new Questao { NomeQuestao = "Como classifica este trilho quanto ao grau de dificuldade?", Desactivada = false, Descricao = "1 - Muito Difícil | 5 - Muito Fácil", OpcaoID = 3 }
                 //new Questao { NomeQuestao = "O guia demonstrou conhecimento do trilho?", Desactivada = false, TipoResposta = "1 - Discordo em absoluto | 5 - Concordo plenamente", ValorMaximo = 5, ValorMinimo = 1, NumeroOpcoes = 5 },
@@ -52,6 +42,26 @@ namespace Trails4Health.Data
                 new Opcao { NumeroOpcoes = 2 },
                 new Opcao { NumeroOpcoes = 3 },
                 new Opcao { NumeroOpcoes = 5 }
+                 );
+                context.SaveChanges();
+            }
+
+            if (!context.Guias.Any())
+            {
+                context.Guias.AddRange(
+                new Guia { Nome = "José Esteves", Telefone = "271100100", Email = "jesteves@gmail.com" },
+                new Guia { Nome = "Túlio Gonzaga", Telefone = "271100200", Email = "tulio@gmail.com" },
+                new Guia { Nome = "António Malaquias", Telefone = "271100300", Email = "amalaquias@gmail.com" }
+                 );
+                context.SaveChanges();
+            }
+
+            if (!context.Turistas.Any()) // Nif's válidos gerados aleatoriamente (https://nif.marcosantos.me/)
+            {
+                context.Turistas.AddRange(
+                new Turista { Nome = "Maurício Abraão", Telefone = "271200100", Email = "mauricio@gmail.com", Nif = 275092879 },
+                new Turista { Nome = "Jaime Coelho", Telefone = "271200200", Email = "jcoelho@gmail.com", Nif = 274261154 },
+                new Turista { Nome = "Pedro Gama", Telefone = "271200300", Email = "mauricio@gmail.com", Nif = 281910430 }
                  );
                 context.SaveChanges();
             }
